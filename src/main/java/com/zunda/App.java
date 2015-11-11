0a01d013f24a03dfe1ef4e4ee3cc9597dfd3aec5
naninuneter55 <naninuneter55@gmail.com>
2015-11-11 07:35:23 +0000

    Add Chap02

--- src/main/java/com/zunda/App.java
+++ src/main/java/com/zunda/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String name = "Koji ONO";
+        System.out.println(name);
     }
 }
---
+++ src/test/java/com/zunda/perfect_java/TestChap02CharacterAndString.java
@@ -0,0 +1,22 @@
+package com.zunda.perfect_java;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
+import static org.hamcrest.CoreMatchers.*;
+/**
+ * Unit test for simple App.
+ */
+public class TestChap02CharacterAndString 
+{
+    @Test
+    public void _02_01_02_01()
+    {
+        String s = new String("0123456789");
+        int actual = s.length();
+        int expected = 10;
+        assertThat(actual, is(expected));
+    }
+
+    
+}
